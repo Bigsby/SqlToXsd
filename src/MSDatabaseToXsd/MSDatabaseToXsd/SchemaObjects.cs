ac566b3b212f051ecac525e3fa02989a79f1b76d
Bruno Bernardo <bbernardo@oneviewhealthcare.com>
2016-05-06 17:54:20 +0100

    Add source code

---
+++ src/MSDatabaseToXsd/MSDatabaseToXsd/SchemaObjects.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace MSDatabaseToXsd
+{
+    public class Schema
+    {
+        public IEnumerable<Table> Tables { get; set; }
+        public IEnumerable<PrimaryKey> PrimaryKeys { get; set; }
+    }
+
+    public class Table
+    {
+        public string Name { get; set; }
+        public IEnumerable<Column> Columns { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Column
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public int? MaxLength { get; set; }
+        public bool Nullable { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {DataType}, {Nullable}, {MaxLength}";
+        }
+    }
+
+    public class PrimaryKey
+    {
+        public string Name { get; set; }
+        public string Table { get; set; }
+        public string Column { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Table}, {Column}";
+        }
+    }
+}
---
+++ src/MSDatabaseToXsd/MSDatabaseToXsd/XmlWriterExtentions.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml;
+
+namespace MSDatabaseToXsd
+{
+    public static class XmlWriterExtensions
+    {
+        public const string SchemaNamespace = "http://www.w3.org/2001/XMLSchema";
+        public const string SchemaNamespacePrefix = "xs";
+
+        public static void WriteStartSchemaElement(this XmlWriter writer, string name)
+        {
+            writer.WriteStartElement(name, SchemaNamespace);
+        }
+    }
+}
